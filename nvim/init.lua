c74f5e9472d8ffe7ad255c9e140203bee2783374
Tobias Johansson <contact@johansson-tobias.com>
2021-07-05 20:36:18 +0200

    Trying to get gdscript to work

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,9 @@ Version:        0.1.0
 Changes:        Trying to convert my init.vim to init.lua
 Windows:        C:\Users\USERNAME\AppData\Local\nvim\
 Linux:          .config\nvim\
+
+Possible plugins
+- https://github.com/ray-x/lsp_signature.nvim
 ---------------------------------------------------------------
 --]]
 
@@ -19,7 +22,7 @@ vim.g.python3_host_prog = "C:/Scoop/apps/python/current/python.exe"
 -- Plugin-manager (paq)
 ---------------------------------------------------------------
 require 'paq-nvim' {
-    'savq/paq-nvim';                  -- Let Paq manage itself
+    'savq/paq-nvim';                  -- Let Paq manage itself, after first install
 ------------------------------------------
 -- Style
 ------------------------------------------
@@ -29,11 +32,11 @@ require 'paq-nvim' {
 -- Development
 ------------------------------------------
     'neovim/nvim-lspconfig';
-    'nvim-lua/completion-nvim';
     'nvim-treesitter/nvim-treesitter';
-    'p00f/nvim-ts-rainbow';
+    'hrsh7th/nvim-compe';
     'windwp/nvim-autopairs';
-    'TimUntersberger/neogit'; 
+    'TimUntersberger/neogit';
+    'kabouzeid/nvim-lspinstall';
 ------------------------------------------
 -- Nice other things
 ------------------------------------------
@@ -43,6 +46,7 @@ require 'paq-nvim' {
     'nvim-lua/popup.nvim';
     'nvim-telescope/telescope.nvim';
     'hoob3rt/lualine.nvim';
+    'habamax/vim-godot';
 }
 
 ---------------------------------------------------------------
@@ -80,6 +84,100 @@ vim.o.expandtab = true
 ------------------------------------------
 -- Plugins
 ------------------------------------------
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.gdscript.setup{}
+
+vim.o.completeopt = "menuone,noselect"
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = false;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    vsnip = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    spell = true;
+    tags = true;
+    snippets_nvim = true;
+    treesitter = true;
+  };
+}
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif vim.fn.call("vsnip#available", {1}) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
+  else
+    -- If <S-Tab> is not working in your terminal, change it to <C-h>
+    return t "<S-Tab>"
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+
+----------------------------------------------------------------
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
 
 ---------------------------------------------------------------
 -- Shortcuts
@@ -115,6 +213,7 @@ map('n', '<Leader>l', '<esc><C-w>r')
 ------------------------------------------
 map('i', '<C-v>', '<esc>"+pi')
 map('v', '<C-c>', '"+y')
+
 
 ------------------------------------------
 -- Adding shortcuts to open files and such
